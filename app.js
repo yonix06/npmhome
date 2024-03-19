a384548257222f7e4360dce724f6354f11718df5
Gareth Billett <billettg@gmail.com>
2024-03-19 10:51:46 +0000

    .

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import config from './config.json' assert { type: 'json'};
 
-const serverHost = config.settings.serverHost;
-const serverPort = config.settings.serverPort;
-const identity = config.settings.email;
-const secret = config.settings.password;
-var toggleDetailEnabled = false;
+const serverHost = config.server.host;
+const serverPort = config.server.port;
+const identity = config.server.email;
+const secret = config.server.password;
+var showForwarding = config.appearance.showforwarding;
+var showDisabled = config.appearance.showdisabled;
+const toggleOnIcon = `<i class="fa-solid fa-toggle-on fa-2xl"></i>`;
+const toggleOffIcon = `<i class="fa-solid fa-toggle-off fa-2xl"></i>`;
 var data;
+
+if (!config.appearance.showheader) document.getElementById("header").style.display = 'none';
+if (!config.appearance.showfooter) document.getElementById("footer").style.display = 'none';
 
 async function getToken() {
     try {
@@ -38,6 +44,7 @@ async function getProxyHosts() {
         });
         if (response.ok) {
             data = await response.json();
+            console.log(data);
             render();
         } else {
             console.log('Error fetching proxy hosts:', response.status);
@@ -50,6 +57,7 @@ async function getProxyHosts() {
 function render() {
     document.getElementById("main").innerHTML = "";
     data.forEach(host => {
+        if (host.enabled == 1 || (host.enabled == 0 && showDisabled)) {
         var fqdn = host.domain_names;
         var forwardHostPort = host.forward_host + ":" + host.forward_port;
         var hostname = fqdn.toString().split('.')[0];
@@ -57,28 +65,34 @@ function render() {
         var forwardScheme = host.forward_scheme;
         const div = document.createElement("div");
         div.innerHTML += `<span class="hostname"> ${hostname}</span><span class="domain">${domain}</span>`
-        if (toggleDetailEnabled) div.innerHTML += `<span class="forwardHostPort"> >> ${forwardHostPort}</span></a>`;
+        if (showForwarding) div.innerHTML += `<span class="forwardingInfo"> >> ${forwardScheme}://${forwardHostPort}</span></a>`;
         div.className = "host-container";
+        if (host.enabled == 0 && showDisabled) div.classList.add("disabled");
         document.getElementById("main").appendChild(div);
         div.addEventListener('click', function(e) {
             window.open(`${forwardScheme}://${fqdn}`);
         })
-    });
-    let toggleDetailIcon;
-    if (toggleDetailEnabled) { 
-        toggleDetailIcon = `<i class="fa-solid fa-toggle-on fa-2xl"></i>`;
-    } else {
-        toggleDetailIcon = `<i class="fa-solid fa-toggle-off fa-2xl"></i>`;
     }
-    document.getElementById("controlIconContainer").innerHTML = toggleDetailIcon;
+    });
+    document.getElementById("showForwardingContainer").innerHTML = showForwarding ? toggleOnIcon : toggleOffIcon;
+    document.getElementById("showDisabledContainer").innerHTML = showDisabled ? toggleOnIcon : toggleOffIcon;
     document.getElementById("serverInfo").innerHTML = `<span id="serverHost">${serverHost}</span>`;
 }
 
-document.getElementById("controlIconContainer").addEventListener('click', function(e) {
-    if (toggleDetailEnabled) { 
-        toggleDetailEnabled = false;
+document.getElementById("showForwardingContainer").addEventListener('click', function(e) {
+    if (showForwarding) { 
+        showForwarding = false;
     } else {
-        toggleDetailEnabled = true
+        showForwarding = true
+    }
+    render();
+});
+
+document.getElementById("showDisabledContainer").addEventListener('click', function(e) {
+    if (showDisabled) { 
+        showDisabled = false;
+    } else {
+        showDisabled = true
     }
     render();
 });
